a650dc6ce6ceaefd04d0dc1a4517134fb3c1cf19
kanishkguptagit <1905902@kiit.ac.in>
2020-11-08 22:34:26 +0530

    resolved hosted database error

--- django_project/settings.py
+++ django_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'careerchela',
+        'USER': 'postgres',
+        'PASSWORD': 'career12',
+        'HOST': '35.236.52.244',
+        'PORT': '5432'
     }
 }
 
